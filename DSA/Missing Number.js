c639ff85cb564c5e20433580104acf72f4bac3e9
55vishal <55.vishalmalusare@gmail.com>
2023-04-15 07:39:32 +0530

    Find missing number

---
+++ DSA/Missing Number.js
@@ -0,0 +1,15 @@
+//Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+
+let nums = [3, 0, 1];
+let desiredSum = 0;
+let currentSum = 0;
+
+for (let i = 0; i <= nums.length; i++) {
+    desiredSum += i;
+    if (i < nums.length) {
+        currentSum += nums[i];
+    }
+}
+
+console.log(desiredSum - currentSum);
+
--- DSA/Palindrome(optimized).js
+++ DSA/Palindrome(optimized).js
@@ -1,9 +1,8 @@
 // Check wethere the given number id palindrome or not.
 
   function isPalindrome(x){
-    
-    return x===+x.toString().split('').reverse().join('')
+   return x<0 ? false : (x === + x.toString().split('').reverse().join(''))
   }
-let res = isPalindrome(121)
-  console.log(res)
   
+  let res = isPalindrome(121)
+  console.log(res)
