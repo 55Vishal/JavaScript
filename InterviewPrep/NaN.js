5b1ddc8b8bd65da199bdbe8fccebb6cd8812fd19
55vishal <55.vishalmalusare@gmail.com>
2023-05-03 08:16:19 +0530

    NaN property in JavaScript.

--- InterviewPrep/NaN.js
+++ InterviewPrep/NaN.js
@@ -1,7 +1,9 @@
 // NaN property in JavaScript.
 
-console.log(isNaN("Hello")) 
+console.log(isNaN("Hello"))
 
-console.log(isNaN(345))  
- 
- console.log(isNaN('1') )  // Returns false
+console.log(isNaN(345))
+
+console.log(isNaN('1'))  // Returns false
+
+console.log(first)
