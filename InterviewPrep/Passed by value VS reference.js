eeb4dcec434fcea75a82672f3110a358ce7605d0
55vishal <55.vishalmalusare@gmail.com>
2023-05-03 08:50:38 +0530

    Passed by value and Passed by reference.

---
+++ InterviewPrep/Passed by value VS reference.js
@@ -0,0 +1,17 @@
+// Passed by value and Passed by reference.
+
+// Passed by value.
+var x=20;
+var y=x
+
+var x=x+5
+
+console.log(x,y)
+
+// Passed by reference.
+
+var obj1 = {name : 'Akash' , surname : 'Roi'}
+var obj2 = obj1
+
+obj1.name= 'Rohan'
+console.log('obj1 :',obj1,'obj2 :', obj2)
