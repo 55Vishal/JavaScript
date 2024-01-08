e10f81c707722e8f9ff0d13933532df8d47d5ecf
vishal.m-at-383439993753 <55.vishalmalusare@gmail.com>
2024-01-08 17:48:43 +0530

    array

--- JavaScript practice/array.js
+++ JavaScript practice/array.js
@@ -1,9 +1,11 @@
 let arr = [100, 20, 651, 400, 500, 600, 700, 550, 30, 80];
 
 function greaterThan200() {
+    let val=[]
     return val > 200;
 }
 function filterExample() {
     let arrFiltered = arr.filter(greaterThan200)
     console.log(arrFiltered)
 }
+filterExample() 
