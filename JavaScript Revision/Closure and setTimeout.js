14b22bd38f96fca0bb59594f54749a44b4b80c40
vishal.m-at-383439993753 <55.vishalmalusare@gmail.com>
2024-01-19 08:01:56 +0530

    setTimeout and closures

---
+++ JavaScript Revision/Closure and setTimeout.js
@@ -0,0 +1,19 @@
+
+function x(){
+    var a = 1;
+    setTimeout(function(){
+     console.log('First function o/p:',a);
+    },3000);
+}
+x();
+
+// Create a function that iterates through the loop from 1 to 5 and prints then within specific interval
+
+function i(){
+    for(let i=1; i<=5; i++){
+        setTimeout(function (){
+        console.log('Second function o/p : ',i)
+        },3000 * i);
+    }
+}
+i();
