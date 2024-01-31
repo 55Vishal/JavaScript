77298b9975aa3c0dda1299fc139c76be42ac5101
vishal.m-at-383439993753 <55.vishalmalusare@gmail.com>
2024-01-31 22:18:29 +0530

    Different method of using function

--- JavaScript Revision/Binary.js
+++ JavaScript Revision/Binary.js
@@ -3,8 +3,18 @@ const arr = [5, 1, 3, 2, 6]
 function binary (x){
   return x.toString(2);
 }
+// 1st method
+const output1  = arr.map(binary);
+console.log('output1',output1);
 
-const output  = arr.map(binary);
+// 2st method
 
-console.log(output);
+const output2  = arr.map(function binary (x){
+    return x.toString(2);
+  });
+console.log('output2',output2);
 
+// 3st method
+
+const output3 = arr.map((x) => x.toString(2))
+console.log('output3',output3);
