d6bf18d0a0c2c754f81200fd372133cef3fd92b6
Vishal. <55.vishalmalusare@gmail.com>
2025-04-02 17:27:43 +0530

    Update Reverse an array.js

--- JavaScript practice/Reverse an array.js
+++ JavaScript practice/Reverse an array.js
@@ -1,6 +1,13 @@
-
+// Reverse an array
+// 1st method
 let arr=[1,2,3,4,5]
-
+let newArr = [];
+for(let i=arr.length-1; i>=0; i--){
+    newArr.push(arr[i)
+}
+console.log(newArr)
+// 2nd method
+let arr=[1,2,3,4,5]
 let i=0;
 let j=arr.length-1;
 
